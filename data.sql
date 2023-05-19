d26cbe18bff5c4979edd81f9558fdcc4bd7c1248
Rana-Mudassir <mduasire38@gmail.com>
2023-05-19 19:08:13 +0500

    Update required Changes

--- data.sql
+++ data.sql
@@ -1,10 +1,10 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, weight_kg, neutered)
- VALUES (1, 'Agumon', 'Feb 3, 2020', 0, 10.23, true);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, weight_kg, neutered)
- VALUES (2, 'Gabumon', 'Nov 15, 2018', 2, 8, true);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, weight_kg, neutered)
- VALUES (3, 'Pikachu', 'Jan 7, 2021', 1, 15.04, false);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, weight_kg, neutered)
- VALUES (4, 'Devimon', 'May 12, 2017', 5, 11, true);
+INSERT INTO animals (name, date_of_birth, escape_attempts, weight_kg, neutered)
+ VALUES ('Agumon', '2020/2/3', 0, 10.23, true);
+INSERT INTO animals (name, date_of_birth, escape_attempts, weight_kg, neutered)
+ VALUES ('Gabumon', '2018/11/15', 2, 8, true);
+INSERT INTO animals (name, date_of_birth, escape_attempts, weight_kg, neutered)
+ VALUES ('Pikachu', '2021/1/7', 1, 15.04, false);
+INSERT INTO animals (name, date_of_birth, escape_attempts, weight_kg, neutered)
+ VALUES ('Devimon', '2017/5/12', 5, 11, true);
--- schema.sql
+++ schema.sql
@@ -1,3 +1,3 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals(id INT,name varchar(20),date_of_birth date, escape_attempts INT, neutered bit, weight_kg decimal(5,2));
+CREATE TABLE animals(id SERIAL PRIMARY KEY, name varchar(20),date_of_birth date, escape_attempts INT, neutered boolean, weight_kg decimal(5,2));
