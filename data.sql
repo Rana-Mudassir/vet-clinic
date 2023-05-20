19b64cbe01f46ba538283a288aff68f8ff8bca08
Rana-Mudassir <mduasire38@gmail.com>
2023-05-21 02:29:10 +0500

    Update Required changes

--- data.sql
+++ data.sql
@@ -64,6 +64,10 @@ INSERT INTO specializations (vet_id, species_id) VALUES(
 (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), (SELECT id FROM species WHERE name = 'Pokemon')
 )
 
+INSERT INTO specializations (vet_id, species_id) VALUES(
+(SELECT id FROM vets WHERE name = 'Stephanie Mendez'), (SELECT id FROM species WHERE name = 'Digimon')
+)
+
 INSERT INTO specializations (vet_id, species_id) VALUES( 
 (SELECT id FROM vets WHERE name = 'Jack Harkness'), (SELECT id FROM species WHERE name = 'Digimon')
 )
