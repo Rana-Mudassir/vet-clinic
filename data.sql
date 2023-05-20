3bef04d77bd9f1927333f1f41bfaeb6c293893ef
Rana-Mudassir <mduasire38@gmail.com>
2023-05-20 15:10:26 +0500

    Add data for new tables

--- data.sql
+++ data.sql
@@ -14,3 +14,37 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Charmander', '2020/2/8', 0,false, -11), ('Plantmon', '2021/11/15', 2, true, -5.7),
 ('Squirtle', '1993/4/2', 3, false, -12.13), ('Angemon', '2005/6/12', 1, true, -45), ('Boarmon', '2005/6/7', 7, true, 20.4),
 ('Blossom', '1998/10/13', 3, true, 17), ('Ditto', '2022/5/14', 4, true, 22);
+
+INSERT INTO species (name) VALUES('Pokemon'),('Digimon');
+
+INSERT INTO owners (full_name, age)  
+VALUES ('Sam Smith', 34), ('Jennifer Orwell', 19), ('Bob', 45), 
+('Melody Pond', 77), ('Dean Winchester', 14), ('Jodie Whittaker', 38);
+
+UPDATE animals
+SET species_id = (SELECT id FROM species WHERE name = 'Digimon')
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species_id = (SELECT id FROM species WHERE name = 'Pokemon')
+WHERE species_id IS NULL;
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE name = 'Sam Smith')
+WHERE name = 'Agumon';
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+WHERE (name = 'Gabumon') OR (name = 'Pikachu');
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob')
+WHERE (name = 'Devimon') OR (name = 'Plantmon');
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean')
+WHERE (name = 'Angemon') OR (name = 'Boarmon');
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+WHERE (name = 'Charmander') OR (name = 'Squirtle') OR (name = 'Blossom');
--- queries.sql
+++ queries.sql
@@ -63,3 +63,4 @@ SELECT species, AVG(escape_attempts) AS avg_attempts
 FROM animals
 WHERE date_of_birth BETWEEN '1990/1/1' AND '2000/12/31'
 GROUP BY species;
+
